de64cce14cb8c7cff523b5c00982d1a377eb403b
Romain Tartiere <romain.tartiere@gmail.com>
2011-04-24 10:21:03 +0000

    Add Microsoft Windows support.
    Update issue 62
    Status: Feedback
    Gleen, I slightly modified your patches to better stick with the style of the
    rest of the project,  Can you please confirm that it does not break?
    
    PR:		Issue 62
    Submitted by:	glenn.ergeerts

---
+++ FindLIBNFC.cmake
@@ -0,0 +1,21 @@
+# $Id$
+# TODO locate using pkg-config for linux/bsd
+
+#set(LIBNFC_INCLUDE_DIRS "")
+#set(LIBNFC_LIBRARIES "")
+set(LIBNFC_INSTALL_DIR $ENV{PROGRAMFILES}/libnfc CACHE PATH "libnfc installation directory")
+
+message("libnfc install dir: " ${LIBNFC_INSTALL_DIR})
+
+find_path(LIBNFC_INCLUDE_DIRS NAMES nfc/nfc.h PATHS ${LIBNFC_INSTALL_DIR}/include)
+message("libnfc include dir found:  " ${LIBNFC_INCLUDE_DIRS})
+
+find_library(LIBNFC_LIBRARIES libnfc PATHS ${LIBNFC_INSTALL_DIR}/lib)
+message("libnfc library found:  " ${LIBNFC_LIBRARIES})
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBNFC DEFAULT_MSG
+  LIBNFC_INCLUDE_DIRS
+  LIBNFC_LIBRARIES
+)
+MARK_AS_ADVANCED(LIBNFC_INCLUDE_DIRS LIBNFC_LIBRARIES)
