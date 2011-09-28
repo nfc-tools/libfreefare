efd0d106bc78473f8d5c1164daed4914894d96ae
Romuald Conty <romuald@libnfc.org>
2011-09-28 15:36:08 +0000

    svn:keywords fix on examples/mifare-classic-read-ndef.c .

--- examples/mifare-classic-read-ndef.c
+++ examples/mifare-classic-read-ndef.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  * 
- * $Id: mifare-classic-write-ndef.c 581 2010-09-07 14:29:11Z rtartiere@il4p.fr $
+ * $Id $
  */
 
 /*
